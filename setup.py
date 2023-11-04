836c70614c007f8ce4df97555dc291f35ca9e885
Vaishnavi Manivannan <65309845+vaishnavimanivannan18@users.noreply.github.com>
2023-11-04 11:59:43 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='personal project',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='vaishnavimanivannan',
+    author_email='',
+    description=''
+)
